2c2c5beb6a2aea2dd8e6c89eb8571afbcc07923f
dmitry <vdi85@mail.ru>
2023-02-27 15:14:17 +0300

    data file added

---
+++ data.js
@@ -0,0 +1,75 @@
+const dataInfo = `
+    [
+
+        {
+            "qotd_date": "2023-02-28T00:00:00.000+00:00",
+            "id": 53168,
+            "dialogue": false,
+            "private": false,
+            "tags": [
+                "society",
+                "time"
+            ],
+            "url": "https://favqs.com/quotes/george-bernard-shaw/53168-do-not-waste--",
+            "favorites_count": 0,
+            "upvotes_count": 1,
+            "downvotes_count": 0,
+            "photo": "https://upload.wikimedia.org/wikipedia/commons/c/ce/George_Bernard_Shaw_notebook.jpg",
+            "author": "George Bernard Shaw",
+            "author_permalink": "george-bernard-shaw",
+            "body": "Do not waste your time on Social Questions. What is the matter with the poor is Poverty; what is the matter with the rich is Uselessness."
+        },
+        {
+            "qotd_date": "2023-02-28T00:00:00.000+00:00",
+            "id": 666,
+            "dialogue": false,
+            "private": false,
+            "tags": [
+                
+            ],
+            "url": "https://favqs.com/quotes/fridtjof-nansen/666-i-demolish-my-b-",
+            "favorites_count": 0,
+            "upvotes_count": 1,
+            "downvotes_count": 0,
+            "photo": "https://upload.wikimedia.org/wikipedia/commons/9/9b/Fridtjof_Nansen_LOC_03377u-3.jpg",
+            "author": "Fridtjof Nansen",
+            "author_permalink": "fridtjof-nansen",
+            "body": "I demolish my bridges behind me, then there is no choice but to move forward."
+        },
+        {
+            "qotd_date": "2023-02-28T00:00:00.000+00:00",
+            "id": 23632,
+            "dialogue": false,
+            "private": false,
+            "tags": [
+                "freedom",
+                "happiness"
+            ],
+            "url": "https://favqs.com/quotes/george-eliot/23632-the-intense-h-",
+            "favorites_count": 0,
+            "upvotes_count": 1,
+            "downvotes_count": 0,
+            "photo": "https://upload.wikimedia.org/wikipedia/commons/4/48/George_Eliot%2C_por_Fran%C3%A7ois_D%27Albert_Durade.jpg",
+            "author": "George Eliot",
+            "author_permalink": "george-eliot",
+            "body": "The intense happiness of our union is derived in a high degree from the perfect freedom with which we each follow and declare our own impressions."
+        },
+        {
+            "qotd_date": "2023-02-28T00:00:00.000+00:00",
+            "id": 7656,
+            "dialogue": false,
+            "private": false,
+            "tags": [
+                "birthday"
+            ],
+            "url": "https://favqs.com/quotes/eleanor-roosevelt/7656-i-think-at-a-c-",
+            "favorites_count": 0,
+            "upvotes_count": 1,
+            "downvotes_count": 0,
+            "photo": "https://upload.wikimedia.org/wikipedia/commons/2/22/Eleanor_Roosevelt_portrait_1933.jpg",
+            "author": "Eleanor Roosevelt",
+            "author_permalink": "eleanor-roosevelt",
+            "body": "I think, at a child's birth, if a mother could ask a fairy godmother to endow it with the most useful gift, that gift should be curiosity."
+        }
+    ]
+`
